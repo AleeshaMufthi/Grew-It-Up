af27351c2ce6a8fcfe932f4de9358e7429d91882
Aleesha Mufthi <aleeshamufthi@gmail.com>
2024-03-11 17:11:10 +0530

    data

--- config/databaseConnect.js
+++ config/databaseConnect.js
@@ -3,19 +3,12 @@ require('dotenv').config(); //Load .env file were mongodb URL is added
 const mongoose = require('mongoose')
 
 // MONGODB CONNECTION
+console.log(process.env.MONGODB_URL,"kkkk");
 
 const dbConnection = async()=>{
     try{
-        await mongoose.connect(process.env.MONGODB_URL,
-        {useNewUrlParser:true,
-        useUnifiedTopology:true})
-
-        mongoose.connection.on('connected',()=>{
-            console.log('connected to mongodb');
-        })
-        mongoose.connection.on('error',(error)=>{
-            console.log('mongodb connection error',error);
-        })
+        const connect= mongoose.connect(`${process.env.MONGODB_URL}`)
+          console.log('Connected to MongoDB');
 
     }catch(error){
         console.log('mongodb connection error',error);
